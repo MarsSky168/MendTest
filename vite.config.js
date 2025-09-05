f28e5a90e31985ed904447c83e40e668bf2e05a5
MarsSky168 <mars.sky@ecelite.com>
2025-09-05 11:18:28 +0800

    test

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  //base: '/Demo/',
+  base: './',
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  // build:{
+  //   emptyOutDir: true,
+  //   outDir: resolve(__dirname,'../Demo')
+  // },
+  build: {
+    target: ['edge90', 'chrome90', 'firefox90', 'safari15']
+  },
+  server: {
+    port: 3001,
+  },
+})
